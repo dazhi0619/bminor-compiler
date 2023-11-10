0941084325a79004f9a2f8f72615747a57fbe857
dazhi0619 <da.zhi0619@gmail.com>
2023-11-09 22:28:17 -0500

    update make test

--- runtest.sh
+++ runtest.sh
@@ -1,30 +1,11 @@
 #!/bin/sh
 
-for testfile in test/print/good*.bminor
+for testfile in test/resolve/good*.bminor
 do
-  ./bminor --print $testfile > $testfile.out1
+  ./bminor --resolve $testfile > $testfile.out1
 done
 
-for testfile in test/print/good*.bminor
+for testfile in test/resolve/good*.bminor
 do
-  ./bminor --print $testfile > $testfile.out2
+  ./bminor --resolve $testfile > $testfile.out2
 done
-
-success=true
-for testfile in test/print/good*.bminor
-do
-  file1=$testfile.out1
-  file2=$testfile.out2
-  diff $file1 $file2 > /dev/null
-  if [ $? != 0 ]
-  then
-    echo "$file1 $file2 don't match!"
-    success=false
-  fi
-done
-if [ $success == true ]
-then
-  echo "success (as expected)"
-else
-  echo "failure (INCORRECT)"
-fi
